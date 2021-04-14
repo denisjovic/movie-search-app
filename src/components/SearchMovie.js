f296f9eb883605936b3d559e28e5042bd9abe999
denisjovic <denis.jovic@tn-tech.co.rs>
2021-04-14 13:18:18 +0200

    First commmit

---
+++ src/components/SearchMovie.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const SearchMovie = () => {
+  return (
+    <div>
+      <form className='form'>
+        <label className='label' htmlFor='query'>
+          Movie name:
+        </label>
+        <input
+          type='text'
+          name='query'
+          id='query'
+          placeholder='i.e Jurassic Park'
+        />
+        <button className='btn'>Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchMovie;
