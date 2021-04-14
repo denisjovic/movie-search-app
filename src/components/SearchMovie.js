d3887d91759ab3229533a40562a015e112a64734
denisjovic <denis.jovic@tn-tech.co.rs>
2021-04-14 14:08:43 +0200

    Move movie card to separate component

--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MovieCard from './MovieCard';
 
 const SearchMovie = () => {
+  const [query, setQuery] = useState('');
+  const [movieData, setMovieData] = useState([]);
+
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=bdcf1fb7aa0d5a4895b44366fbaa92e3&language=en-US&query=${query}&page=1&include_adult=false`;
+  const searchMovie = async (e) => {
+    e.preventDefault();
+    try {
+      let response = await fetch(url);
+      let data = await response.json();
+      setMovieData(data.results);
+    } catch (err) {
+      console.err(err);
+    }
+  };
+
   return (
     <div>
-      <form className='form'>
+      <form className='form' onSubmit={searchMovie}>
         <label className='label' htmlFor='query'>
           Movie name:
         </label>
@@ -13,9 +29,18 @@ const SearchMovie = () => {
           id='query'
           placeholder='i.e Jurassic Park'
           className='input'
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
         />
         <button className='btn'>Search</button>
       </form>
+      <div className='card-list'>
+        {movieData
+          .filter((movie) => movie.poster_path)
+          .map((movie) => {
+            return <MovieCard movie={movie} key={movie.id} />;
+          })}
+      </div>
     </div>
   );
 };
